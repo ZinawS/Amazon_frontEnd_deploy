309f373150afc5ed465ae62d43b667ee7cf7f66b
ZinawS <Zinshol@hotmail.com>
2025-05-31 16:48:43 -0400

    Amazon deployement frontend rev08

--- src/Utility/firebase.jsx
+++ src/Utility/firebase.jsx
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: "clone-69546.firebaseapp.com",
-  projectId: "clone-69546",
-  storageBucket: "clone-69546.appspot.com", // Fixed storage bucket URL
-  messagingSenderId: "70411282250",
-  appId: "1:70411282250:web:7691c6562c84b0784703b3",
-  measurementId: "G-STB5S70CD5",
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: "G-STB5S70CD5", // This can remain hardcoded if you prefer
 };
 
 // Initialize Firebase
